7637561d9a39526131e609ffa6e487f95d05d36e
BraveJero <jbrave271@gmail.com>
2023-05-01 23:08:38 -0300

    Add MLP reference online

--- src/simple_perceptron.py
+++ src/simple_perceptron.py
@@ -63,3 +63,62 @@ class SimplePerceptron:
         data = np.insert(data, 0, 1, axis=1)
         h = np.dot(data, self._weights)
         return self._activation_function.evaluate(h)
+
+
+class MLP:
+    def __init__(self, input_size: int, output_size: int, hidden_layers: List[int],
+                 activation_function: ActivationMethod, optimization_method: OptimizationMethod,
+                 cut_condition: CutCondition, epochs: int):
+        self._input_size = input_size
+        self._output_size = output_size
+        self._hidden_layers = hidden_layers
+        self._activation_function = activation_function
+        self._optimization_method = optimization_method
+        self._cut_condition = cut_condition
+        self._epochs = epochs
+
+        # Initialize weights with random values between -1 and 1
+        sizes = [input_size] + hidden_layers + [output_size]
+        self._weights = [np.random.uniform(-1, 1, (sizes[i] + 1, sizes[i + 1])) for i in range(len(sizes) - 1)]
+
+    def train_batch(self, data: ndarray[float], answers: ndarray[float]) -> int:
+        # Add a 1 for w0
+        data = np.insert(data, 0, 1, axis=1)
+
+        # assert data and answers dimensions are correct
+        assert data.shape[0] == answers.shape[0]
+        assert data.shape[1] == self._weights[0].shape[0]
+
+        for epoch in range(self._epochs):
+            activations = [data]
+            h = data
+
+            # Forward pass
+            for i in range(len(self._weights)):
+                h = np.dot(h, self._weights[i])
+                results = self._activation_function.evaluate(h)
+                activations.append(results)
+                h = np.insert(results, 0, 1, axis=1)  # Add bias for next layer
+
+            derivatives = self._activation_function.d_evaluate(h)
+            errors = answers - activations[-1]
+            deltas = [errors * derivatives]
+
+            # Backward pass
+            for i in range(len(self._weights) - 1, 0, -1):
+                delta = np.dot(deltas[-1], self._weights[i].T)
+                delta = delta[:, 1:]  # Remove bias
+                derivative = self._activation_function.d_evaluate(activations[i])
+                delta *= derivative
+                deltas.append(delta)
+
+            deltas.reverse()
+
+            # Update weights
+            for i in range(len(self._weights)):
+                self._weights[i] += self._optimization_method.adjust(deltas[i], activations[i], data)
+
+            if self._cut_condition.is_finished(errors):
+                return epoch
+
+        return self._epochs
