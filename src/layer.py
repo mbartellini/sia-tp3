e20f75ddc5082b82e6bfbd854a73607dd366bf86
BraveJero <jbrave271@gmail.com>
2023-05-02 16:10:25 -0300

    type the neuron

--- src/layer.py
+++ src/layer.py
@@ -1,7 +1,10 @@
+from numpy import ndarray
+
+
 class Layer:
-    def __init__(self, neurons):
+    def __init__(self, neurons: ndarray[float]):
         self._neurons = neurons
 
     @property
-    def neurons(self):
+    def neurons(self) -> ndarray[float]:
         return self._neurons
