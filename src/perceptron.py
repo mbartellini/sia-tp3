127f3a43dad6d3c4e9a54e2d31bc797a33d6aa56
BraveJero <jbrave271@gmail.com>
2023-05-01 23:09:01 -0300

    Update perceptron structure and complete constructor

--- src/perceptron.py
+++ src/perceptron.py
@@ -4,46 +4,58 @@ from typing import List
 import numpy as np
 
 from src.activation_method import ActivationMethod
+from src.cut_condition import CutCondition
 from src.error_method import ErrorMethod
+from src.layer import Layer
 from src.optimization_method import OptimizationMethod
 from src.update_method import UpdateMethod
 
 
 class Perceptron(ABC):
-    # TODO
     def __init__(self,
-                 dimension: int,
                  activation_method: ActivationMethod,
                  error_method: ErrorMethod,
                  learn_rate: float,
                  epochs: int,
                  update_method: UpdateMethod,
-                 layer_sizes: List[int],
+                 architecture: List[int],
                  optimization_method: OptimizationMethod,
-                 initial_weights: Supplier[float]
+                 cut_condition: CutCondition
                  ):
-        self._weights = np.zeros(dimension) # TODO: Check +1 for w_0
+
+        # Initialize weights for the whole network with random [1-0] values.
+        self._layers = []
+        for i in range(len(architecture) - 1):
+            self._layers.append(Layer(np.random.rand(architecture[i], architecture[i+1])))  # TODO: Discuss bias
+            
+        self._update_method = update_method
+        self._error_method = error_method
+        self._learn_rate = learn_rate
         self._activation_function = activation_method
+        self._epochs = epochs
+        self._cut_condition = cut_condition
+        self._activation_function = activation_method
+        self._optimization_method = optimization_method
 
-    def predict(self, stimuli: List[Number]) -> Number:
-        return self._activation_function.evaluate(
-            np.dot([-1]+stimuli, self._weights)
-        )
-
-    def train(self,
-              learning_rate: Number,
-              epoch_limit: int,
-              update_method: UpdateMethod,
-              cut_condition: CutCondition,
-              data):
-        for _ in range(epoch_limit):
-            self._epoch(learning_rate, data)
-            if cut_condition.is_finished():
-                # TODO check cost or error function and break
-
-    def _epoch(self,
-               learning_rate: Number,
-               data):
-        for d in data:
-            p = self.predict(d.stimuli)
-            dw = learning_rate * (d.output - p) * np.asarray(d.stimuli)
+    # def predict(self, stimuli: List[Number]) -> Number:
+    #     return self._activation_function.evaluate(
+    #         np.dot([-1]+stimuli, self._weights)
+    #     )
+    #
+    # def train(self,
+    #           learning_rate: Number,
+    #           epoch_limit: int,
+    #           update_method: UpdateMethod,
+    #           cut_condition: CutCondition,
+    #           data):
+    #     for _ in range(epoch_limit):
+    #         self._epoch(learning_rate, data)
+    #         if cut_condition.is_finished():
+    #             # TODO check cost or error function and break
+    #
+    # def _epoch(self,
+    #            learning_rate: Number,
+    #            data):
+    #     for d in data:
+    #         p = self.predict(d.stimuli)
+    #         dw = learning_rate * (d.output - p) * np.asarray(d.stimuli)
