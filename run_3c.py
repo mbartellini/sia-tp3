f01f96a0b33a5b0e4cb8982d3037d93dc520c093
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-05-03 00:04:15 -0300

    Implement runner for exercise 3c

---
+++ run_3c.py
@@ -0,0 +1,36 @@
+import sys
+
+import numpy as np
+
+import utils
+from run_3b import get_numbers
+from src.multi_layer_perceptron import MultiLayerPerceptron
+
+
+def run_3_a():
+    if len(sys.argv) < 2:
+        print("Config file argument not found")
+        exit(1)
+
+    config_path = sys.argv[1]
+    settings = utils.get_settings(config_path)
+    cut_condition = utils.get_cut_condition(settings)
+    activation_method = utils.get_activation_function(settings)
+    optimization_method = utils.get_optimization_method(settings)
+    epochs = utils.get_epochs(settings)
+
+    X = get_numbers("data/TP3-ej3-digitos.txt")
+    EXPECTED = np.array([[int(i == j) for j in range(10)] for i in range(len(X))])
+
+    perceptron = MultiLayerPerceptron([X.shape[1], 100, 25, EXPECTED.shape[1]],
+                                      epochs,
+                                      cut_condition,
+                                      activation_method,
+                                      optimization_method)
+    print(f"Training finished in {perceptron.train_batch(X, EXPECTED)} epochs.")
+
+    print([np.argmax(test) for test in perceptron.predict(X)])
+
+
+if __name__ == "__main__":
+    run_3_a()
