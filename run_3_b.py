eb73bc3400c5788fe7fa4bda8f1c30ad07eb3503
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-05-02 23:41:33 -0300

    Implement runner for exercise 3b

---
+++ run_3_b.py
@@ -0,0 +1,50 @@
+import sys
+
+import numpy as np
+from numpy import ndarray
+
+import utils
+from src.multi_layer_perceptron import MultiLayerPerceptron
+
+
+def get_numbers(path: str) -> ndarray[float]:
+    with open(path, "r") as f:
+        list_of_lines = f.read().splitlines()
+        print(list_of_lines)
+        list_of_lines_without_spaces = [np.fromstring(line, dtype=int, sep=' ') for line in list_of_lines]
+        print(list_of_lines_without_spaces)
+        numbers = np.array(
+            list(zip(*(iter(list_of_lines_without_spaces),) * (len(list_of_lines_without_spaces) // 10))))
+        print(numbers)
+    return np.array([number.ravel() for number in numbers])
+
+
+def run_3_a():
+    if len(sys.argv) < 2:
+        print("Config file argument not found")
+        exit(1)
+
+    config_path = sys.argv[1]
+    settings = utils.get_settings(config_path)
+    cut_condition = utils.get_cut_condition(settings)
+    activation_method = utils.get_activation_function(settings)
+    optimization_method = utils.get_optimization_method(settings)
+    epochs = utils.get_epochs(settings)
+
+    X = get_numbers("data/TP3-ej3-digitos.txt")
+    EXPECTED = np.array([[1 - 2 * (i % 2)] for i in range(len(X))])
+    print(X)
+
+    perceptron = MultiLayerPerceptron([X.shape[1], 100, 25, EXPECTED.shape[1]],
+                                      epochs,
+                                      cut_condition,
+                                      activation_method,
+                                      optimization_method)
+    print(f"Training finished in {perceptron.train_batch(X, EXPECTED)} epochs.")
+
+    ans = perceptron.predict(X)
+    print(ans)
+
+
+if __name__ == "__main__":
+    run_3_a()
